22df5e30f779445d37be625126ca87944caa771d
Prerana07 <preranapandit07@gmail.com>
2019-11-25 12:03:54 +0545

    One picture integrated with name and contact number

--- app/src/main/java/com/example/aad_topic5/MainActivity.java
+++ app/src/main/java/com/example/aad_topic5/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.aad_topic5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //create lists of contacts to display in Recycler view
+        List<Contacts> contactsList = new ArrayList<>();
+        contactsList.add(new Contacts("Nature pic","9851001818",R.drawable.image1));
+
+        ContactsAdapter contactsAdapter = new ContactsAdapter(this,contactsList);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
