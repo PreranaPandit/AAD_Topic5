198c386909cce5e9ca4fa4b2953020c4b727dd9d
PreranaPandit <preranapandit07@gmail.com>
2019-12-07 18:08:37 +0545

    Circular image view completion

--- app/src/main/java/com/example/aad_topic5/MainActivity.java
+++ app/src/main/java/com/example/aad_topic5/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
         //create lists of contacts to display in Recycler view
         List<Contacts> contactsList = new ArrayList<>();
-        contactsList.add(new Contacts("Nature pic","9851001818",R.drawable.image1));
+        contactsList.add(new Contacts("Martin Luis","9851001818",R.drawable.momj));
+        contactsList.add(new Contacts("Michael Jordon","98546431311",R.drawable.celb));
+        contactsList.add(new Contacts("Martin Brat","98545451231",R.drawable.eagle));
+
+
 
         ContactsAdapter contactsAdapter = new ContactsAdapter(this,contactsList);
         recyclerView.setAdapter(contactsAdapter);
